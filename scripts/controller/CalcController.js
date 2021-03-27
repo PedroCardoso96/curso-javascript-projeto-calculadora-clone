69d75a4d2dc4fb09be300f7438f5a9ee2dd50922
PedroCardoso96 <pedrohfsc96@gmail.com>
2021-03-27 17:07:45 -0300

    update

--- scripts/controller/CalcController.js
+++ scripts/controller/CalcController.js
@@ -74,16 +74,28 @@ class CalcController {
         let result = eval(this._operation.join(""));
 
         this._operation = [result, last];
+
+        this.setLastNumberToDisplay();
     }
 
 
 
     setLastNumberToDisplay(){
 
-        
+        let lastNumber;
+
+        for(let i = this._operation.length-1; i >= 0; i--){
+            
+            if(!this.isOperator(this._operation[i])){
+                lastNumber = this._operation[i];
+                break;
+            }
+        }
+
+        this._displayCalc = lastNumber;
     }
 
-    addOparation(value){
+    addOperation(value){
 
         
 
@@ -97,6 +109,8 @@ class CalcController {
                 console.log('Outra coisa', value);
             } else {
                 this.pushOperation(value);
+
+                this.setLastNumberToDisplay();
             }
 
         } else {
@@ -137,31 +151,31 @@ class CalcController {
                 break;
 
             case 'soma':
-                this.addOparation('+');                
+                this.addOperation('+');                
                 break;
 
             case 'subtracao':
-                this.addOparation('-');
+                this.addOperation('-');
                 break;
 
             case 'divisao':
-                this.addOparation('/');
+                this.addOperation('/');
                 break;
 
             case 'multiplicacao':
-                this.addOparation('*');
+                this.addOperation('*');
                 break;
 
             case 'porcento':
-                this.addOparation('%');
+                this.addOperation('%');
                 break;
 
             case 'ponto':
-                this.addOparation('.');
+                this.addOperation('.');
                 break;
 
             case 'igual':
-                this.addOparation('=');
+                this.addOperation('=');
                 break;
 
             case '0':
@@ -174,7 +188,7 @@ class CalcController {
             case '7':
             case '8':
             case '9':
-                this.addOparation(parseInt(value));
+                this.addOperation(parseInt(value));
                 break;
 
             default:
